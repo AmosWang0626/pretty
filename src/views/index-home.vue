ad55ba629f3e937bdbaba9d05ab9dbef1af2c916
daoyuan <dywang@firstqianbao.com>
2018-03-29 22:59:13 +0800

    优化主题

--- src/views/index-home.vue
+++ src/views/index-home.vue
@@ -1,89 +1,42 @@
 <style scoped>
     .layout {
-        border: 1px solid #d7dde4;
         background: #f5f7f9;
         position: relative;
-        border-radius: 4px;
         overflow: hidden;
     }
 
-    .layout-logo {
-        width: 100px;
-        height: 30px;
-        background: #5b6270;
-        border-radius: 3px;
-        float: left;
-        position: relative;
-        top: 15px;
-        left: 20px;
-    }
-
     .layout-nav {
-        width: 420px;
-        margin: 0 auto;
-        margin-right: 20px;
+        width: 380px;
+        float: right;
+        position: relative;
     }
 
     .layout-footer-center {
         text-align: center;
     }
-
-    .layout-con {
-        height: 100%;
-        width: 100%;
-    }
-
-    .menu-item span {
-        display: inline-block;
-        overflow: hidden;
-        width: 69px;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        vertical-align: bottom;
-        transition: width .2s ease .2s;
-    }
-
-    .menu-item i {
-        transform: translateX(0px);
-        transition: font-size .2s ease, transform .2s ease;
-        vertical-align: middle;
-        font-size: 16px;
-    }
-
-    .collapsed-menu span {
-        width: 0px;
-        transition: width .2s ease;
-    }
-
-    .collapsed-menu i {
-        transform: translateX(5px);
-        transition: font-size .2s ease .2s, transform .2s ease .2s;
-        vertical-align: middle;
-        font-size: 22px;
-    }
 </style>
-
 <template>
     <div class="layout">
         <Layout>
-            <Header :style="{ boxShadow: '0 2px 3px 2px rgba(0,0,0,.1)'}">
-                <Menu mode="horizontal" theme="dark" active-name="1">
-                    <div class="layout-logo"></div>
+            <Header :style="{background: getColor(pageTheme)}">
+                <Menu :theme="pageTheme" mode="horizontal" active-name="1">
                     <div class="layout-nav">
                         <MenuItem name="1">
                             <Icon type="ios-navigate"></Icon>
                             Item 1
                         </MenuItem>
                         <MenuItem name="2">
-                            <Icon type="ios-keypad"></Icon>
-                            Item 2
+                            <Dropdown trigger="click" @on-click="changeTheme">
+                                <Icon type="ios-analytics"></Icon>
+                                切换主题
+                                <DropdownMenu slot="list">
+                                    <DropdownItem name="light">象牙白</DropdownItem>
+                                    <DropdownItem name="dark">高雅灰</DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
                         </MenuItem>
                         <MenuItem name="3">
-                            <Icon type="ios-analytics"></Icon>
-                            Item 3
-                        </MenuItem>
-                        <MenuItem name="4">
-                            <Dropdown>
+                            <Dropdown trigger="click">
                                 <Avatar style="background-color: #87d068" size="large">{{nickName}}</Avatar>
                                 <Icon type="arrow-down-b"></Icon>
                                 <DropdownMenu slot="list">
@@ -92,57 +45,76 @@
                                     <DropdownItem divided disabled>设置</DropdownItem>
                                 </DropdownMenu>
                             </Dropdown>
-
                         </MenuItem>
                     </div>
                 </Menu>
             </Header>
 
-            <Layout :style="{minHeight: '95vh'}">
-                <Sider collapsible :collapsed-width="78" v-model="isCollapsed">
-                    <Menu active-name="1-2" theme="dark" width="auto" :class="menuItemClasses">
-                        <MenuItem name="1-1">
-                            <Icon type="ios-navigate"></Icon>
-                            <span>Option 1</span>
-                        </MenuItem>
-                        <MenuItem name="1-2">
-                            <Icon type="search"></Icon>
-                            <span>Option 2</span>
-                        </MenuItem>
-                        <MenuItem name="1-3">
-                            <Icon type="settings"></Icon>
-                            <span>Option 3</span>
-                        </MenuItem>
-                    </Menu>
-                </Sider>
-
-                <Content :style="{padding: '16px 16px 16px'}">
-                    <!--<Breadcrumb :style="{margin: '16px 0'}">-->
-                    <!--<BreadcrumbItem>Home</BreadcrumbItem>-->
-                    <!--<BreadcrumbItem>Components</BreadcrumbItem>-->
-                    <!--<BreadcrumbItem>Layout</BreadcrumbItem>-->
-                    <!--</Breadcrumb>-->
-                    <Card>
-                        <div style="height: 700px">Content</div>
-                    </Card>
-                    <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
+            <Layout :style="{padding: '0 50px'}">
+                <Content :style="{padding: '24px 0', minHeight: '280px', background: '#fff'}">
+                    <Layout>
+                        <Sider hide-trigger :style="{background: '#fff'}">
+                            <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']" :theme="pageTheme">
+                                <Submenu name="1">
+                                    <template slot="title">
+                                        <Icon type="ios-navigate"></Icon>
+                                        Item 1
+                                    </template>
+                                    <MenuItem name="1-1">Option 1</MenuItem>
+                                    <MenuItem name="1-2">Option 2</MenuItem>
+                                    <MenuItem name="1-3">Option 3</MenuItem>
+                                </Submenu>
+                                <Submenu name="2">
+                                    <template slot="title">
+                                        <Icon type="ios-keypad"></Icon>
+                                        Item 2
+                                    </template>
+                                    <MenuItem name="2-1">Option 1</MenuItem>
+                                    <MenuItem name="2-2">Option 2</MenuItem>
+                                </Submenu>
+                                <Submenu name="3">
+                                    <template slot="title">
+                                        <Icon type="ios-analytics"></Icon>
+                                        Item 3
+                                    </template>
+                                    <MenuItem name="3-1">Option 1</MenuItem>
+                                    <MenuItem name="3-2">Option 2</MenuItem>
+                                </Submenu>
+                            </Menu>
+                        </Sider>
+                        <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
+                            Content
+                        </Content>
+                    </Layout>
                 </Content>
-
             </Layout>
+            <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
         </Layout>
     </div>
 </template>
-
 <script>
     export default {
         data() {
             return {
                 isCollapsed: false,
-                nickName: 'Amos'
+                nickName: 'Amos',
+                pageTheme: 'dark'
             };
         },
+        methods: {
+            changeTheme: function (index) {
+                this.pageTheme = index;
+            },
+            getColor(colorName) {
+                if (colorName === 'light') {
+                    return '#fff';
+                } else {
+                    return '#495060';
+                }
+            }
+        },
         computed: {
-            menuItemClasses: function () {
+            menuitemClasses: function () {
                 return [
                     'menu-item',
                     this.isCollapsed ? 'collapsed-menu' : ''
